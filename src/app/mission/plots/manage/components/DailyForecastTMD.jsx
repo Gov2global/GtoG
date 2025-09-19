d6762016f1b6e738c0b3eb41047ea5eec8151fa1
jobigboss <josursit@gmail.com>
2025-09-19 12:56:16 +0700

    up

--- src/app/mission/plots/manage/components/DailyForecastTMD.jsx
+++ src/app/mission/plots/manage/components/DailyForecastTMD.jsx
@@ -7,7 +7,7 @@ export default function DailyForecastTMD({ lat, lon }) {
   useEffect(() => {
     async function fetchForecast() {
       try {
-        const token = "Bearer ใส่ token ที่คุณมี"; // 👈 ใส่ token ที่ถูกต้องตรงนี้
+        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImJmOGFhNGQ0MDgzOGMyOTE1ODAxYTk0N2QzYjI4ZjFjYmQyNGUzYTQxODUyODQ4YzY3OGQ4ZjNiYTMwNTJmYWNlZjJjZjRkZjEzOTNiZTMwIn0.eyJhdWQiOiIyIiwianRpIjoiYmY4YWE0ZDQwODM4YzI5MTU4MDFhOTQ3ZDNiMjhmMWNiZDI0ZTNhNDE4NTI4NDhjNjc4ZDhmM2JhMzA1MmZhY2VmMmNmNGRmMTM5M2JlMzAiLCJpYXQiOjE3NTgyMzEyMTgsIm5iZiI6MTc1ODIzMTIxOCwiZXhwIjoxNzg5NzY3MjE4LCJzdWIiOiI0Mzc3Iiwic2NvcGVzIjpbXX0.gd1HH62Pzf3MLa4oQA6Nj-jcS-JURSveAhf14gf8rEsVrScz-Y5R6OGFtRAqGaD_NTuV93uxJKnvDaNVT68CqHvVsb4OQBL-poY6fz7OhG1IVPIV56bYCHX2PsBqOcRar8AOan7Ri4y0y6eds3DMoCfVuJArf8Kbqpy0CLTsVG1F4ezqavozSfhi4RK_6__25742JqyVK8Mt3aFAjrLs8jB3dzeUTQ8SnN7ACyyrfq1-rUtN8iLI6dFVnNQBlescAh1LDoPNicSQn0_fIJ1wAR-pCdsMIrng6_sMiJDdE1JN8yQ_lPQmbdzyKL38c5BkyBbDs1mUlg_UTLoM2D5mKpOO2FFkCEdllEYJSctpWrYvEshtAdrlcB1caWpK9916M9ER-dEEKqAm2d60nibwmSK4ofzU9zcuV7Www9YXwszeHu48pKNVncp0fRYVPxME0bXU4e5T9Xjhn8He7LjRedb5ipObgkB5Pte-Hnbxnw4Etur14BrNyDHNBUcr0AivkRk9Rrxnfd2qGwzs4CDL1-j08CCJ5zpdOmBnqDR2L7TRyG66eP3ivEZvYSw1cHBOoy8OGqLrKerjVrfse_y0NyS-abNQ5-ByL8jnCZ0wLpUDxMdJOYDGlRkxOLtnMjGrbvYT7KfHaxXqLQmmPXvzf5TUzre6h3FYJ6ErK8T6LBY"; // 👈 ใส่ token ที่ถูกต้องตรงนี้
         const today = new Date().toISOString().split("T")[0]
 
         const res = await fetch(
