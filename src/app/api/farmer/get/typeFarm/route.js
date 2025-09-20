23f27e24dea6732a8b887ea3c457bb9c5b169e56
jobigboss <josursit@gmail.com>
2025-09-20 11:53:35 +0700

    200968-1153

--- src/app/api/farmer/get/typeFarm/route.js
+++ src/app/api/farmer/get/typeFarm/route.js
@@ -3,28 +3,6 @@ import { connectMongoDB } from '../../../../../../lib/mongodb1';
 import TypeFarm from '../../../../../../models/typeFarm';
 import { NextResponse } from "next/server";
 
-// export async function GET() {
-//   try {
-//     await connectMongoDB();
-
-//     // ✅ ดึงข้อมูลทั้งหมดของ typeFarm
-//     const typeFarmList = await TypeFarm.find().sort({ typeID: 1 });
-
-//     return NextResponse.json({
-//       success: true,
-//       data: typeFarmList,
-//     }, { status: 200 });
-
-//   } catch (error) {
-//     console.error("❌ ดึงข้อมูล typeID ไม่สำเร็จ:", error);
-//     return NextResponse.json({
-//       success: false,
-//       message: "เกิดข้อผิดพลาดในการดึงข้อมูลประเภทฟาร์ม",
-//     }, { status: 500 });
-//   }
-// }
-
-
 export async function GET() {
   try {
     await connectMongoDB();
@@ -42,7 +20,6 @@ export async function GET() {
     return NextResponse.json({
       success: false,
       message: "เกิดข้อผิดพลาดในการดึงข้อมูลประเภทฟาร์ม",
-      error: error.message,
     }, { status: 500 });
   }
 }
