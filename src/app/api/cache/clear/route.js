5ce8895de1f7778aa590e22152a90cfd46b8ac76
jobigboss <josursit@gmail.com>
2025-09-21 11:58:03 +0700

    210968-1158

--- src/app/api/cache/clear/route.js
+++ src/app/api/cache/clear/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { clearCache } from "@/lib/cache";
+import { clearCache } from "../../../../../lib/cache";
 
 export async function GET(req) {
   try {
